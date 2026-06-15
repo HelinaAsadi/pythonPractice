afae13353394480b3d05077f868690e008321180
Helina <heliii.asadi@gmail.com>
2026-06-15 14:08:42 +0330

    initil commit

---
+++ basics.py
@@ -0,0 +1,30 @@
+# VARIABLES : container for vales. 4 types of variables are string, integer, float, boolean
+
+# strings
+first_name = "Helina"
+last_name = 'Asadi'
+
+print(first_name)
+ # using f string to pring a combo of given texts and variables
+print(f"Hello there {first_name} {last_name}")
+
+
+# itegers (whole numbers)
+age = 19
+
+# float (floating point numberwith a decimal proportion)
+distance_to_dreams = 1090373093900373191.9729
+
+
+#boolean
+happy = False
+depressed = True
+if happy :
+    print("yaaaay :)")
+else:
+    print("poor you :(")
+
+
+
+
+
